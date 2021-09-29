daf9b09280ebf4171466cf3cd1fd1b5b5eba8941
Joel Beckum <joelbeckum@gmail.com>
2021-09-29 14:58:20 -0500

    Added Tag Model, Repos, and Controller; implemented Index action

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public List<Tag> GetAll()
+        { 
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name
+                                        FROM Tag
+                                        ORDER BY Name";
+
+                    List<Tag> tags = new List<Tag>();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Tag tag = new Tag()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+
+                            tags.Add(tag);
+                        }
+
+                        return tags;
+                    }
+                }
+            }
+        }
+    }
+}
